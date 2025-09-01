10138f76bd8a06715e4e737cf39f52070e682c3e
1ats <job.alberto.soki@gmail.com>
2025-09-01 20:12:54 -0300

    update

--- src/services/pdfService.js
+++ src/services/pdfService.js
@@ -24,35 +24,8 @@ async function generatePdfBuffer(receiptData) {
 }
 
 function renderHTML(receiptData) {
-  const templatePath = path.join(__dirname, "..", "templates", "generalReceiptTemplate.html");
-  let html = fs.readFileSync(templatePath, "utf-8");
-  html = html
-    .replace("{{receiptId}}", receiptData.receiptId)
-    .replace("{{date}}", receiptData.date)
-    .replace("{{customerName}}", receiptData.customer.name)
-    .replace("{{customerEmail}}", receiptData.customer.email)
-    .replace("{{customerPhone}}", receiptData.customer.phone)
-    .replace("{{discount}}", receiptData.discount)
-    .replace("{{tax}}", receiptData.tax)
-    .replace("{{paymentMethod}}", receiptData.payment_method)
-    .replace("{{total}}", receiptData.total_amount);
 
-  const itemRows = receiptData.items
-    .map(
-      (item) => `
-      <tr>
-        <td>${item.name}</td>
-        <td>${item.quantity}</td>
-        <td>${item.price}</td>
-        <td>${item.discount}</td>
-        <td>${item.tax}</td>
-      </tr>`
-    )
-    .join("");
-
-  html = html.replace(/<tbody>.*<\/tbody>/s, `<tbody>${itemRows}</tbody>`);
-
-  return html;
+  return 'html';
 }
 
 module.exports = {
