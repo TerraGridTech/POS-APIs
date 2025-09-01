367ef8d84b33401a83477a01d7b588ce1ccc6680
1ats <job.alberto.soki@gmail.com>
2025-09-01 17:58:47 -0300

    update

--- src/services/qrCodeService.js
+++ src/services/qrCodeService.js
@@ -1,18 +1,8 @@
 const QRCode = require('qrcode');
 
 async function generateQrCodeBuffer(transactionId) {
-  if (!transactionId || typeof transactionId !== 'string') {
-    throw new Error('transactionId inválido para gerar QR Code');
-  }
-
-  const url = `https://tgtpos-api-b9eqafawaabtf6ev.centralindia-01.azurewebsites.net/receipt/${transactionId}`;
-  
-  try {
-    return await QRCode.toBuffer(url, { type: 'png' });
-  } catch (error) {
-    console.error('Erro ao gerar QR Code:', error.message);
-    throw error; // ou retorne null se quiser ignorar
-  }
+  const url = `https://tgtpos-api-b9eqafawaabtf6ev.centralindia-01.azurewebsites.net/api/transactions/${transactionId}`;
+  return await QRCode.toBuffer(url, { type: 'png' });
 }
 
 module.exports = {
