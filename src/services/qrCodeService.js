b9bb48e83c1a2b1c4de5d6561c0ad80fdc4ac823
1ats <job.alberto.soki@gmail.com>
2025-09-01 18:25:48 -0300

    update

--- src/services/qrCodeService.js
+++ src/services/qrCodeService.js
@@ -1,8 +1,8 @@
 const QRCode = require('qrcode');
 
 async function generateQrCodeBuffer(transactionId) {
-//  const url = `https://tgtpos-api-b9eqafawaabtf6ev.centralindia-01.azurewebsites.net/api/transactions/${transactionId}`;
-//  return await QRCode.toBuffer(url, { type: 'png' });
+  const url = `https://tgtpos-api-b9eqafawaabtf6ev.centralindia-01.azurewebsites.net/api/transactions/${transactionId}`;
+  return await QRCode.toBuffer(url, { type: 'png' });
 return 1;
 }
 
