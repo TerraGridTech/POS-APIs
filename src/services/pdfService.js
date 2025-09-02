b70cf585eebf150bb27f0d924c09ee1b1aee7647
1ats <job.alberto.soki@gmail.com>
2025-09-02 14:21:27 -0300

    update

--- src/services/pdfService.js
+++ src/services/pdfService.js
@@ -29,7 +29,7 @@ async function generatePdfBuffer(receiptData) {
             '--no-sandbox',
             '--disable-setuid-sandbox'
         ],
-       executablePath: path.resolve(__dirname, '../.cache/puppeteer/.../chrome-linux/chrome')
+       executablePath: path.resolve(__dirname, './chromium/chrome')
        });
 
     const page = await browser.newPage();
