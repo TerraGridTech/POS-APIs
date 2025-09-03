13872ca5c5fc5508f8949c73afb1b4d300e2cb5b
1ats <job.alberto.soki@gmail.com>
2025-09-03 02:15:39 -0300

    update

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 // src/server.js
 const app = require('./app');
 
- const PORT = process.env.PORT || 3002;
+ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`POS Data Ingestion Service running on port ${PORT}`);
